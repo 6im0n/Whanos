b4706f8d9f15c88641260f4c870de37047ee153f
6im0n <simon.gl3135@orange.fr>
2024-12-01 20:16:16 +0100

    :lock: exemple file of vars to full before use terraform

---
+++ Jenkins/variables.tf
@@ -0,0 +1,14 @@
+variable "project_id" {
+  description = "The GCP project ID"
+  type        = string
+}
+
+variable "service_account_key_path" {
+  description = "Path to the service account key file"
+  type        = string
+}
+
+variable "service_account_email" {
+  description = "Service account email"
+  type        = string
+}
